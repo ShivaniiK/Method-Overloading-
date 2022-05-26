f6ab7aa4e18461a751c0113c9cd095fc9c576bb4
ShivaniiK <105714949+ShivaniiK@users.noreply.github.com>
2022-05-26 07:19:26 -0700

    Add files via upload

---
+++ MethodOverload.java
@@ -0,0 +1,15 @@
+package com.MethodOverloading;     //MethodOverloading
+
+	public class MethodOverload
+	{
+public void product(int alpha,float beta)
+{
+    System.out.println("Total Multiplication is: "+alpha*beta);
+}  
+ 
+public void product (int sum1,int sum2,int sum3)
+{
+    System.out.println("Total Multiplication is: "+sum1*sum2*sum3);
+}  
+}
+	
---
+++ TestMethodOverload.java
@@ -0,0 +1,11 @@
+package com.MethodOverloading;
+
+public class TestMethodOverload {
+
+	public static void main(String[] args) {
+		MethodOverload objmethodOverload = new MethodOverload();
+		objmethodOverload.product(2,2);
+	    objmethodOverload.product(2,2,2);
+	}
+
+}
